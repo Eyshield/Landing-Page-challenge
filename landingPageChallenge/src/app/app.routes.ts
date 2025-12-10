a53f1f9504b54e6923bf956806207fa210070c71
Eyshield <samy.amaouche008@gmail.com>
2025-12-10 17:30:24 +0100

    ended

---
+++ landingPageChallenge/src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { LandinPage } from './landin-page/landin-page';
+
+export const routes: Routes = [{ path: '', component: LandinPage }];
---
+++ landingPageChallenge/src/app/landin-page/landin-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NavBar } from '../nav-bar/nav-bar';
+
+@Component({
+  selector: 'app-landin-page',
+  imports: [NavBar],
+  templateUrl: './landin-page.html',
+  styleUrl: './landin-page.css',
+})
+export class LandinPage {}
